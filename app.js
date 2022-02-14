ad8b47d2f1ba7c9b614e3319dce2b5ef8138bfa4
nnabicse <nnabi.cse@gmail.com>
2022-02-14 15:32:13 +0600

    update product qty complete

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
-console.log('I am in a separate file')
+function updateProductNumber(product,price,isIncreasing){
+    const productInput = document.getElementById(product+"-number");
+    let productNumber = productInput.value;
+    if(isIncreasing){
+        productNumber = parseInt(productNumber) + 1
+    }
+    else if(productNumber>0){
+        productNumber = parseInt(productNumber) - 1
+    }
+    productInput.value = productNumber;
+
+    const productTotal =  document.getElementById(product+"-price-total");
+    productTotal.innerText = productNumber*price;
+}
+
+
+
+document.getElementById("phone-plus").addEventListener("click", function(){
+    updateProductNumber("phone",1219,true);
+    
+});
+
+
+document.getElementById("phone-minus").addEventListener("click", function(){
+    updateProductNumber("phone",1219,false);
+    
+});
+
+document.getElementById("case-plus").addEventListener("click", function(){
+
+    updateProductNumber("case",59,true);
+
+});
+
+
+document.getElementById("case-minus").addEventListener("click", function(){
+    updateProductNumber("case",59,false);
+    
+});
+
+
+
